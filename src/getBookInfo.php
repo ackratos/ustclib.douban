d3f2d615a770fe3bba3943f1c35ceb8d527513e7
zhaocong Mac <zhaocong89@gmail.com>
2013-04-02 10:01:47 +0800

    server code refactor

--- src/getBookInfo.php
+++ src/getBookInfo.php
@@ -18,63 +18,47 @@
 		echo "targetUrl = " . $targetUrl;
 	}
 	$html = file_get_html($targetUrl);
+	$exactUrl = "";
 	$resultArray = array();
-	foreach($html->find('li[class=book_list_info]') as $bookInfo){
-		$url = "http://opac.lib.ustc.edu.cn/opac/" . $bookInfo->children(0)->children(1)->href;
-		if($DEBUG){
-			echo "<br/>" . $url;
+	function retriveBookInfo($html, $step){
+		foreach($html->find('li[class=book_list_info]') as $bookInfo){
+			$url = "http://opac.lib.ustc.edu.cn/opac/" . $bookInfo->children(0)->children(1)->href;	
+			if($step == 1 && $url == $GLOBALS['exactUrl']){
+				continue;
+			}
+			if($step == 0){
+				$GLOBALS['exactUrl'] = $url;
+			}
+			if($DEBUG){
+				echo "<br/>" . $url;
+			}
+			$title = $bookInfo->children(0)->children(1)->innertext;
+			$title = preg_split('/[0-9]+[.]/', $title);
+			$title = $title[1];
+			if($DEBUG){
+				echo "<br/>" . $title;
+			}
+			$accessible = $bookInfo->children(1)->children(0)->innertext;
+			$accessible = preg_split('/可借复本：/', $accessible);
+			if($DEBUG){
+				echo "<br/>" . $accessible[1];
+			}
+			$totalcount = $accessible[0];
+			$totalcount = preg_split('/馆藏复本：/', $totalcount);
+			preg_match('/[0-9]+/', $totalcount[1], $matches);
+			if($DEBUG){
+				echo "<br/>" . $matches[0];
+			}
+			$accessible = $accessible[1];
+			$totalcount = $matches[0];
+			$arr = array('url'=>$url, 'title'=>$title, 'accessible'=>$accessible, 'totalcount'=> $totalcount);
+			array_push($GLOBALS['resultArray'], $arr);
 		}
-		$title = $bookInfo->children(0)->children(1)->innertext;
-		$title = preg_split('/[0-9]+[.]/', $title);
-		$title = $title[1];
-		if($DEBUG){
-			echo "<br/>" . $title;
-		}
-		$accessible = $bookInfo->children(1)->children(0)->innertext;
-		$accessible = preg_split('/可借复本：/', $accessible);
-		if($DEBUG){
-			echo "<br/>" . $accessible[1];
-		}
-		$totalcount = $accessible[0];
-		$totalcount = preg_split('/馆藏复本：/', $totalcount);
-		preg_match('/[0-9]+/', $totalcount[1], $matches);
-		if($DEBUG){
-			echo "<br/>" . $matches[0];
-		}
-		$accessible = $accessible[1];
-		$totalcount = $matches[0];
-		$arr = array('url'=>$url, 'title'=>$title, 'accessible'=>$accessible, 'totalcount'=> $totalcount);
-		array_push($resultArray, $arr);
 	}
-
+	retriveBookInfo($html, 0);
 	$targetUrl = $USTC_LIB_QUERY_URL . "?title=" . $bookname;
 	$html = file_get_html($targetUrl);
-	foreach($html->find('li[class=book_list_info]') as $bookInfo){
-		$url = "http://opac.lib.ustc.edu.cn/opac/" . $bookInfo->children(0)->children(1)->href;
-		if($DEBUG){
-			echo "<br/>" . $url;
-		}
-		$title = $bookInfo->children(0)->children(1)->innertext;
-		$title = preg_split('/[0-9]+[.]/', $title);
-		$title = $title[1];
-		if($DEBUG){
-			echo "<br/>" . $title;
-		}
-		$accessible = $bookInfo->children(1)->children(0)->innertext;
-		$accessible = preg_split('/可借复本：/', $accessible);
-		if($DEBUG){
-			echo "<br/>" . $accessible[1];
-		}
-		$totalcount = $accessible[0];
-		$totalcount = preg_split('/馆藏复本：/', $totalcount);
-		preg_match('/[0-9]+/', $totalcount[1], $matches);
-		if($DEBUG){
-			echo "<br/>" . $matches[0];
-		}
-		$accessible = $accessible[1];
-		$totalcount = $matches[0];
-		$arr = array('url'=>$url, 'title'=>$title, 'accessible'=>$accessible, 'totalcount'=> $totalcount);
-		array_push($resultArray, $arr);
-	}
+	retriveBookInfo($html, 1);	
 	echo "processRelatedResult(".json_encode($resultArray).")";
+
 ?>
